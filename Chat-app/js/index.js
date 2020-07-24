3a7f772d0bcf1ebee1d3823849dc2d6502970178
khiemnb <khiemnb2705@gmail.com>
2020-07-24 20:18:58 +0700

    init firebase

--- Chat-app/js/index.js
+++ Chat-app/js/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  console.log('Windown loaded')
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA5y7z5vHLPUaSQxQjJp8agm93j-dgHNM0",
+    authDomain: "chat-app-aed27.firebaseapp.com",
+    databaseURL: "https://chat-app-aed27.firebaseio.com",
+    projectId: "chat-app-aed27",
+    storageBucket: "chat-app-aed27.appspot.com",
+    messagingSenderId: "318133683614",
+    appId: "1:318133683614:web:f39b23275de951caf4428f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase.app().name)
   view
   .setActiveScreen('registerScreen')
 }
